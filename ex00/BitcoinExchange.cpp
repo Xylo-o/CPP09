eb2693fe4b309adc0ca0c8523b6bb3b6909e5274
Xyloo <adprzyby@student.42heilbronn.de>
2025-01-28 17:23:56 +0100

    Added empty file to check + improved logic for checking input file

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,17 +6,28 @@
 /*   By: adprzyby <adprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 14:34:48 by adprzyby          #+#    #+#             */
-/*   Updated: 2025/01/27 16:46:43 by adprzyby         ###   ########.fr       */
+/*   Updated: 2025/01/28 17:20:18 by adprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
 bool checkFile(const std::string& str) {
-	if (str.find(".txt") != std::string::npos) {
-		return true;
-	}
-	return false;
+    const std::string extension = ".txt";
+    if (str.size() >= extension.size() && str.compare(str.size() - extension.size(), extension.size(), extension) == 0) {
+        std::ifstream file(str);
+        if (!file.is_open()) {
+            std::cerr << RED << "Error: " << NC "failed to open file." << std::endl;
+            return false;
+        }
+        if (file.peek() == std::ifstream::traits_type::eof()) {
+            std::cerr << RED << "Error: " << NC << "file is empty." << std::endl;
+            return false;
+        }
+        file.close();
+        return true;
+    }
+    return false;
 }
 
 std::string trim(const std::string& str) {
@@ -37,7 +48,7 @@ float calculateResult(const std::string& ammount, float exchangeRate) {
 int parseExchangeDatabase(const std::string& data, std::map<std::string, float>& dataBase) {
     std::ifstream file(data);
     if (!file.is_open()) {
-        std::cerr << "Error: Failed to open file." << std::endl;
+        std::cerr << RED << "Error: " << NC << "Failed to open file." << std::endl;
         return 1;
     }
     std::string line;
@@ -61,22 +72,29 @@ int parseExchangeDatabase(const std::string& data, std::map<std::string, float>&
             float value = std::stof(valueStr);
             dataBase[date] = value;
         } catch (const std::exception& e) {
-            std::cerr << "Error: Failed to process value in line: " << line << " (" << e.what() << ")" << std::endl;
+            std::cerr << RED << "Error: " << NC << "Failed to process value in line: " << line << " (" << e.what() << ")" << std::endl;
         }
     }
     file.close();
     return 0;
 }
 
-bool validateLine(const std::string& date, const std::string& value, const std::string& line) {
-    std::regex datePattern(R"(\d{4}-\d{2}-\d{2})");
-    std::regex floatPattern(R"([-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?)");
+bool validateLine(const std::string& line, std::string& date, std::string& value) {
     std::regex formatPattern(R"(\d{4}-\d{2}-\d{2} \| [-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?)");
-	if (!std::regex_match(line, formatPattern)) {
+    if (!std::regex_match(line, formatPattern)) {
         std::cerr << RED << "Error: " << NC << "bad input => " << line << std::endl;
         return false;
     }
-	if (!std::regex_match(date, datePattern)) {
+    std::istringstream lineStream(line);
+    if (!std::getline(lineStream, date, '|') || !std::getline(lineStream, value)) {
+        std::cerr << RED << "Error: " << NC << "bad input => " << line << std::endl;
+        return false;
+    }
+    date = trim(date);
+    value = trim(value);
+    std::regex datePattern(R"(\d{4}-\d{2}-\d{2})");
+    std::regex floatPattern(R"([-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?)");
+    if (!std::regex_match(date, datePattern)) {
         std::cerr << RED << "Error: " << NC << "invalid date " << date << std::endl;
         return false;
     }
@@ -98,7 +116,7 @@ bool validateLine(const std::string& date, const std::string& value, const std::
 int parseInputFile(const std::string& data, std::map<std::string, float>& dataBase) {
     std::ifstream file(data);
     if (!file.is_open()) {
-        std::cerr << "Error: failed to open file." << std::endl;
+        std::cerr << RED << "Error: " << NC << "failed to open file." << std::endl;
         return 1;
     }
     std::string line;
@@ -112,28 +130,21 @@ int parseInputFile(const std::string& data, std::map<std::string, float>& dataBa
             continue;
         }
         line = trim(line);
-        std::istringstream lineStream(line);
         std::string date, valueStr;
-        if (!std::getline(lineStream, date, '|') || !std::getline(lineStream, valueStr)) {
-            std::cerr << RED << "Error: " << NC << "bad input => " << line << std::endl;
-            continue;
-        }
-        date = trim(date);
-        valueStr = trim(valueStr);
-        if (!validateLine(date, valueStr, line)) {
+        if (!validateLine(line, date, valueStr)) {
             continue;
 		}
         auto it = dataBase.lower_bound(date);
         if (it == dataBase.end() || it->first != date) {
             if (it == dataBase.begin()) {
-                std::cerr << "Error: no exchange rate found for date: " << date << std::endl;
+                std::cerr << RED << "Error: " << NC << "no exchange rate found for date: " << date << std::endl;
                 continue;
             }
             --it;
         }
         float exchangeRate = it->second;
         float result = calculateResult(valueStr, exchangeRate);
-        std::cout << date << " => " << valueStr << " * " << exchangeRate << " = " << result << std::endl;
+        std::cout << GREEN << date << " => " << NC << valueStr << " * " << exchangeRate << " = " << result << std::endl;
     }
     file.close();
     return 0;
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,17 +6,18 @@
 /*   By: adprzyby <adprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 14:39:18 by adprzyby          #+#    #+#             */
-/*   Updated: 2025/01/26 22:35:54 by adprzyby         ###   ########.fr       */
+/*   Updated: 2025/01/28 17:22:15 by adprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
 int main(int argc, char** argv) {
-	if (argc != 2 || !checkFile(argv[1])) {
-		std::cerr << RED << "Error: " << NC << "could not open the file." << std::endl;
+	if (argc != 2) {
+		std::cerr << RED << "Error: " << NC << "worng usage.\nCorrect usage: ./btc <file_name.txt>" << std::endl;
 		return 1;
 	}
+	checkFile(argv[1]);
 	std::map<std::string, float> database;
 	if (parseExchangeDatabase("data.csv", database) != 0) {
 		std::cerr << RED << "Error: " << NC << "could not load the database." << std::endl;
